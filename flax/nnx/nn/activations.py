7355baf03493a5398b2ef115886bcfa9e1d8eb71
Ojas Patil <129142263+sanepunk@users.noreply.github.com>
2025-10-20 14:07:52 +0000

    Added Parametric Swish Activation

--- flax/nnx/nn/activations.py
+++ flax/nnx/nn/activations.py
@@ -115,3 +115,41 @@ class PReLU(nnx.Module):
       inputs,
       jnp.asarray(self.negative_slope[...], inputs.dtype) * inputs,
     )
+
+class PSwish(nnx.Module):
+  """Parametric Swish activation function.
+
+  Note that PSwish is a Flax layer and not a simple activation function, so
+  it needs to be initialized before being called.
+
+  Example::
+
+    >>> import flax.nnx as nnx
+
+    >>> class MLP(nnx.Module):
+    ...   def __init__(self):
+    ...     self.linear = nnx.Linear(3, 2)
+    ...     self.act = nnx.PSwish(beta_init=1.0)
+    ...
+    ...   def __call__(self, x):
+    ...     x = self.linear(x)
+    ...     x = self.act(x)
+    ...     return x
+
+  Args:
+    beta_init: the value to initialize the beta parameter (default 1.0).
+    param_dtype: the dtype passed to parameter initializers (default: float32).
+  """
+  def __init__(
+    self,
+    beta_init: float = 1.0,
+    param_dtype: Dtype = jnp.float32
+  ):
+    self.beta = nnx.Param(
+      jnp.asarray(beta_init, dtype=param_dtype)
+    )
+    self.param_dtype = param_dtype
+
+  def __call__(self, inputs: Array) -> Array:
+    beta = jnp.asarray(self.beta[...], inputs.dtype)
+    return inputs * sigmoid(beta * inputs)
